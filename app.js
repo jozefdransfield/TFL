ec1eae6a9038165f1f9201c7d9dfce996a6da819
Jozef Dransfield <Jozef.Dransfield@me.com>
2012-07-20 01:28:57 +0100

    web interface

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 var express= require("express");
 var http = require("http");
 var url = require("url");
-var TFLBusService = require("./tfl");
+var TFLBusService = require("./lib/tfl");
+
+var app = express.createServer();
+app.set("views", __dirname + "/view");
+app.set('partials'   , __dirname + '/views/partials');
+app.set("view engine", "ejs");
+
+app.configure(function () {
+    app.use(express.logger('\x1b[33m:method\x1b[0m \x1b[32m:url\x1b[0m :response-time'));
+    app.use(express.cookieParser());
+    app.use(express.session({secret:'session-id'}));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+});
 
 var bus = new TFLBusService();
 
-var app = express.createServer();
+
 
 app.get('/busstop/:busstop', function(req, res) {
 	bus.findByBusStop(req.params.busstop, function(data) {
 		res.send(data);
 	});
+});
+
+app.get("/", function(req, res){
+   res.render("timer")
 });
 
 function fetchAndParseBusTimes() {
@@ -22,7 +42,6 @@ function fetchAndParseBusTimes() {
             setTimeout(fetchAndParseBusTimes, 30000);
 		});          
 	});
-
 }
 
 fetchAndParseBusTimes();
